37e3539b5e9ce07924893066ede4f5ef2c18b258
Stjepan Labor <stjepan.labor24@gmail.com>
2025-03-29 16:12:41 +0100

    contact-message

--- Contact.js
+++ Contact.js
@@ -1,6 +1,26 @@
-$(document).ready(function () {
-  $("#contact-form").on("submit", function () {
-    let checkbox = $("#ReceiveNewsletter");
-    checkbox.val(checkbox.is(":checked") ? "true" : "false");
+jQuery(document).ready(function () {
+  //kada se cijeli dokument ucita
+  jQuery("#contact-form").submit(function (e) {
+    var form = jQuery(this);
+
+    // Provjerava jeli je checkbox označen i postavlja vrijednost
+    var receiveNewsletter = jQuery("#receiveNewsletter").prop("checked")
+      ? "true"
+      : "false";
+
+    // Sakupljanje svih podataka sa forme i dodavanje ReceiveNewsletter
+    var formData = form.serializeArray(); // Koristimo serializeArray da dobijemo podatke u objektima
+    formData.push({ name: "ReceiveNewsletter", value: receiveNewsletter }); // Dodajemo checkbox vrijednost
+
+    jQuery.ajax({
+      url: "https://www.fulek.com/mvc/supit/project-contact-form",
+      type: "POST",
+      data: formData, // Šaljemo podatke
+      success: function (data) {},
+      error: function (data) {
+        console.log("Greška");
+        console.log(data);
+      },
+    });
   });
 });
